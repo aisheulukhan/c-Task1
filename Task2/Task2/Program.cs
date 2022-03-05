b4166574751e71658c037ffc48adf8864283a48a
Aysha Ulukhanova <tu7hnsxs1@aztu.local>
2022-03-05 13:20:37 +0400

    Added Task2 file

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Metni daxil edin");
+            text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++ )
+            {
+                if (text [i] =='a')
+                {
+                    Console.WriteLine("Metnde a simvolu var");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Metnde a simvolu yoxdur");
+                    continue;
+                }
+                
+            }
+            
+
+
+        }
+    }
+}
