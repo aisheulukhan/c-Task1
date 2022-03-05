c36317cd09fac3886c4b0abf7182476f0d6cdc59
tu7hnsxs1 <tu7hnsxs1@code.edu.az>
2022-03-05 10:00:03 +0400

    Task1 file

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 9, -5, 0, 76, 34, 6, -18 };
+            int min = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Arrayin en kicik elementi:" + min);
+        }
+    }
+}
